fa63cf24089f319e6b00c48977b04351bc35c71c
Brendon Ho <brendonho01@users.noreply.github.com>
2015-06-05 21:32:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-/*
-*/
-//Buttons Array
+//Button Array
 var buttons = [];
-var maker = string:"Brendon Ho";
-var helper = string: "Prafull Sharma"
-//Make KeysIs(Pressed)(Released)
-
-
-var currentScene;
-
-
-var entranceScreen = function(){
-  fill(255, 102, 0)
-  currentScene = 1;
-}
-mouseClicked = function
+var leftButton = function(){
+//player Variables
+var gravity = 0.4;
+var positionX = 100 
