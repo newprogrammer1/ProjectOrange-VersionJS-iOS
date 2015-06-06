2986f92341e284a6ccc104c6ec9e19dcb4abee12
Brendon Ho <brendonho01@users.noreply.github.com>
2015-06-05 21:33:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 //Button Array
 var buttons = [];
 var leftButton = function(){
+if(mousePressed && mouseX)
+};
+var rightButton = function(){
+  
+};
+var upButton = function(){
+  
+};
 //player Variables
 var gravity = 0.4;
 var positionX = 100 
