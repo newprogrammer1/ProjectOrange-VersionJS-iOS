467b0ae2758f0b247d4396fa5da53844a8d02acf
Brendon Ho <brendonho01@users.noreply.github.com>
2015-06-05 21:53:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-//Button Array
+//Button Array & Buttons
 var buttons = [];
 var leftButton = function(){
-if(mousePressed && mouseX)
+  rect(374,379,21,15);
 };
 var rightButton = function(){
-  
+  rect(326,379,21,15);
 };
 var upButton = function(){
-  
+  rect(358,379,15,15);
 };
+if(mousePressed && mouseX <=,mouseX >=, mouseY <=, mouseY >=){
+  positionX + 1;
+}else if{
+  if(mousePressed && mouseX <=, mouseX >=, mouseY <=, mouseY >=){
+    positionX - 1;
+  }
+  
+}else if{
+  if(mousePressed && mouseX <=, mouseX >=, mouseY <= mouseY >=){
+    positionY + 3
+  }
+}
+
 //player Variables
 var gravity = 0.4;
-var positionX = 100 
+var positionX = 100;
+var positionY = 0;
+var velocityX = 0;
+var velocityY = 0;
+var maxFallSpeed = 8:
+var ballSize = 18;
