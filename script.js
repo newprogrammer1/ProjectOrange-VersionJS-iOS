cf13506953c978512b3d9f65c599d1d1f7966b46
Brendon Ho <brendonho01@users.noreply.github.com>
2015-06-05 22:19:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,18 +9,6 @@ var rightButton = function(){
 var upButton = function(){
   rect(358,379,15,15);
 };
-if(mousePressed && mouseX <=,mouseX >=, mouseY <=, mouseY >=){
-  positionX + 1;
-}else if{
-  if(mousePressed && mouseX <=, mouseX >=, mouseY <=, mouseY >=){
-    positionX - 1;
-  }
-  
-}else if{
-  if(mousePressed && mouseX <=, mouseX >=, mouseY <= mouseY >=){
-    positionY + 3
-  }
-}
 
 //player Variables
 var gravity = 0.4;
@@ -30,3 +18,47 @@ var velocityX = 0;
 var velocityY = 0;
 var maxFallSpeed = 8:
 var ballSize = 18;
+var jumping = true;
+//platform array
+var platforms = [];
+//creates the platforms functions
+var makePlatforms = function(x,y,width,height,color){
+  platforms.push({
+    x: x, y: y,
+    width: width, height: height,
+    color: color
+  });
+}
+//draws the platforms
+var drawPlatforms = function(){
+  for(var each in platforms){
+    fill(platforms[each].color)
+    rect(platforms[each].x, platforms[each].y, platforms[each].width, platforms[each].height);
+  }
+};
+
+
+//Allows Player to move with controls
+var movePlayer = function(){
+  if(mousePressed && mouseX <=, mouseX >=, mouseY <=, mouseY >=){velocityX = 3;}
+  if(mousePressed && mouseX <=, mouseX >=, mouseY <=, mouseY >=){velocityX = -3;}
+  if(mousePressed && mouseX <=, mouseX >=, mouseY <=, mouseY >=, !jumping){velocityY = -8:}
+  velocityY += gravity;
+
+if(velocityY > maxFallSpeed) {
+    velocityY = maxFallSpeed; }
+    
+positionX += velocityX;
+
+collideWith(velocityX, 0, platforms);
+    
+positionY += velocityY;
+
+jumping = true;
+collideWith(0, velocityY, platforms);
+}
+
+var player = function(){
+  noStroke();
+  fill()
+};
